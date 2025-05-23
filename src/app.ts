4d6b01a7566b201385f237d1a8f818855efd516a
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-23 15:10:20 +0200

    feat: add decoration field and decoration methods

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-//@Loggin("Logging - Person")
+@Loggin("Logging - Person")
 @WithTemplate("<h1>My person Object</h1>", "app")
 class Person {
   name = "Nik";
@@ -27,3 +27,42 @@ class Person {
 }
 
 const pers = new Person();
+
+console.log(pers);
+
+//----
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
