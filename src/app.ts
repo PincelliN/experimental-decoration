1b2d84ea503b388efb8ecf29bd91f95f344732f1
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-23 09:53:37 +0200

    fix: add with tamplate decorator to create a title in the Dom and print in a class Person Name

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,12 @@ function Loggin(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (_: Function) {
+  return function (constructor: any) {
     const hookEl = document.getElementById(hookId);
+    const p = new constructor();
     if (!!hookEl) {
       hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
   };
 }
