ae7ff1798da93bd78131693222e766ea9b509176
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-23 09:44:19 +0200

    feat: create a decorator to add at the DOM a title

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-function Loggin(constructor: Function) {
-  console.log("Loggin...");
-  console.log(constructor);
+function Loggin(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Loggin
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (!!hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
+}
+
+//@Loggin("Logging - Person")
+@WithTemplate("<h1>My person Object</h1>", "app")
 class Person {
   name = "Nik";
 
